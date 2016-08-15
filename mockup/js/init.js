1c6ea5899266ac765d4c192cdf8a2a58b8a441ca
Edgar Cruzado <edgar@redisrupt.io>
2016-08-15 17:39:48 -0500

    application's mockup

---
+++ mockup/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    Materialize.updateTextFields();
+    $('select').material_select();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
