ef2b0d62b7011e82d03262efae3340195589e734
Edgar Cruzado <edgar@redisrupt.io>
2016-08-18 18:03:32 -0500

    modify nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,12 @@ const Header = () => {
             <nav className="top-nav light-green">
                 <div className="nav-wrapper container">
                     <a href="#" className="brand-logo">COOKBOOK</a>
+                  
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-                        <li><Link to="/recipe" activeClassName="active">Recipe</Link></li>
-                        <li><Link to="/about" activeClassName="active">About</Link></li> 
-                    </ul>            
+                        <li><input id="search" type="search" required/></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>                   
                 </div>
             </nav>
         </header>
