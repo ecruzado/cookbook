78a9e59db8a2f642dde3a293ad21d9d8136c1483
Edgar <edgarivan.cruzado@gmail.com>
2016-08-19 04:11:03 -0500

    clean project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
-import '../static/libs/materialize/materialize.css';
+import '../node_modules/materialize-css/dist/css/materialize.css';
 import './styles/styles.css';
 import $ from "jquery";
 import Materialize from 'materialize-css';
