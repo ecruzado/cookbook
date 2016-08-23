28ee694868f894e85ac487af8737676043f60424
Edgar <edgarivan.cruzado@gmail.com>
2016-08-23 09:40:01 -0500

    refactor header

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, {PropTypes}  from 'react';
 import {connect} from 'react-redux';
-import Header from './HeaderSearch';
+import Header from './Header';
 import Footer from './Footer';
 
 class App extends React.Component {
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import {Link, IndexLink } from 'react-router';
+import {connect} from 'react-redux';
+import { setNameFilter } from '../actions/recipeActions';
 
-const Header = ({onNameSearch}) => {
-    return (
-        <header>
-            <nav className="top-nav light-green">
-                <div className="nav-wrapper container">
-                    <a href="#" className="brand-logo">COOKBOOK</a>
-                  
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><input id="search" type="search" placeholder="Recipe Name" onKeyPress={onNameSearch}/></li>
-                        <li><a href="sass.html"><i className="material-icons">search</i></a></li> 
-                    </ul>                   
-                </div>
-            </nav>
-        </header>
-    );
-};
-
-export default Header;
+@connect(
+    (state, ownProps) => ({}),
+    (dispatch, ownProps) => ({
+        onNameSearch: (event) => {
+            if(event.key === 'Enter'){
+                dispatch(setNameFilter(event.target.value));
+            }
+        }
+    })    
+)
+export default class Header extends React.Component {
+    render(){
+        return (
+            <header>
+                <nav className="top-nav light-green">
+                    <div className="nav-wrapper container">
+                        <a href="#" className="brand-logo">COOKBOOK</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li>
+                                <input id="search" type="search" placeholder="Recipe Name" 
+                                    onKeyPress={this.props.onNameSearch}/>
+                            </li>
+                            <li>
+                                <a href="sass.html"><i className="material-icons">search</i></a>
+                            </li> 
+                        </ul>                   
+                    </div>
+                </nav>
+            </header>
+        );        
+    }
+}
